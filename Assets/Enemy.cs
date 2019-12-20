0cb95b66157ef895aa6eac3f494c87eefe2ee85a
蔡遵造 <caizunzao@4399inc.com>
2019-12-20 17:56:17 +0800

    142 控制滴人死亡

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,13 +6,82 @@ public class Enemy : MonoBehaviour
 {
 
     public GameObject damageEffectPrefab;
-    private Transform bloodPoint;
+    private Transform bloodPoint; //喷血的位置
+    private CharacterController cc;
 
     public int hp = 200;
+    public float speed = 2; //移动速度
+    public float downSpped = 1f;//移到地下速度
+    private float downDistance = 0; //移到地下距离
+    public int attackRate = 2; //攻击速率 多少秒攻击一次
+    private float attackTimer = 0;//攻击计时
+    private float distance; //与主角的距离
+    public float attackDistance = 2; //攻击距离
 
     void Start()
     {
         bloodPoint = transform.Find("BloodPoint");
+        cc = GetComponent<CharacterController>();
+        InvokeRepeating("CalcDistance", 0, 0.1f); //获取和主角的距离，每隔0.1s获取一次，不需要再update获取，节约性能
+    }
+
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            //移到地下
+            downDistance += downSpped * Time.deltaTime;
+            transform.Translate(-transform.up * downSpped * Time.deltaTime);
+            if (downDistance > 4)
+            {
+                Destroy(gameObject);
+            }
+            return;
+        }
+        Animation anim = GetComponent<Animation>();
+        if (distance < attackDistance)
+        {
+            attackTimer += Time.deltaTime;
+            if (attackTimer >= attackRate)
+            {
+                //面向主角
+                Transform player = TranscriptManager._instance.player.transform;
+                Vector3 targetPos = player.position;
+                targetPos.y = transform.position.y;
+                transform.LookAt(targetPos);
+
+                //攻击
+                anim.Play("attack01");
+
+                attackTimer = 0;
+            }
+
+            if (!anim.IsPlaying("attack01"))
+            {
+                anim.CrossFade("idle");
+            }
+        }
+        else
+        {
+            anim.Play("walk");
+            Move();
+        }
+
+    }
+
+    void Move()
+    {
+        Transform player = TranscriptManager._instance.player.transform;
+        Vector3 targetPos = player.position;
+        targetPos.y = transform.position.y;
+        transform.LookAt(targetPos);
+        cc.SimpleMove(transform.forward * speed);
+    }
+
+    void CalcDistance()
+    {
+        Transform player = TranscriptManager._instance.player.transform;
+        distance = Vector3.Distance(player.position, transform.position);
     }
 
     //受到攻击调用方法
@@ -44,6 +113,6 @@ public class Enemy : MonoBehaviour
 
     void Dead()
     {
-
+        GetComponent<Animation>().Play("die");
     }
 }
